52188bd1034462bc28b29283bb2d7a5f7931d0b9
İlker Gül <ilkergul@sabanciuniv.edu>
2022-12-23 19:48:06 +0300

    initial commit

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "The Internet and American Beer Heritage",
+        "excerpt":"Beer A word that carries memories of aromas, foam, good times, and a tad bit of a bitter taste. Is there anything better than cracking a cold beer on a sweaty, thirsty hot day at the beach, or chugging on IPA while cheering for your favorite sports team? The answer...","categories": ["jekyll update","Layout"],
+        "tags": [],
+        "url": "/american-beer-heritage",
+        "teaser": "/assets/images/beerUS.png"
+      }]
